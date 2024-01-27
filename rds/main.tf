f3ba1041c5d0a88afdf8e93c2a2b27c53ccab5dc
sitoro4ka <148403485+sitoro4ka@users.noreply.github.com>
2024-01-27 13:15:13 -0500

    added

--- rds/main.tf
+++ rds/main.tf
@@ -5,5 +5,4 @@ resource "aws_db_subnet_group" "default" {
     data.terraform_remote_state.this.outputs.private_subnets[1],
     data.terraform_remote_state.this.outputs.private_subnets[2]
     ]
-  ]
 }
